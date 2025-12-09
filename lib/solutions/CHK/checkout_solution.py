ab7ad6330f525106bdbbaf9be0af9f84abd3919d
sbx_user1051 <sbx_user1051@169.254.54.117>
2025-12-09 11:08:29 +0000

    Tue Dec 09 11:08:29 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -14,33 +14,44 @@ class CheckoutSolution:
         "C" : [(2,40, ("B",1))]
     }
 
-    def offerCalculator(self, item, qty):
+    def offerCalculator(self, item, qty, itemsOrdered):
         regularPrice = self.prices[item]
         offers = self.specialOffers[item]
 
         # 1) evaluate the quantity against each offer to calculate best possible deal
         # 2) deduct the price of any freebies (if in the basket)
         totalPrice = 0
+        qtyRemaining = qty # claim offers greedily
         for offer in offers:
             if qty < offer[0]: # not enough bought to claim the offer
                 totalPrice += (regularPrice * qty)
+                break
             elif qty == offer[0]: # quantity matches offer perfectly
                 totalPrice += offer[1]
-          
-            else: # can claim at least one offer
-                numOffer = qty // offer[0]
-                remaining = qty - (numOffer*offer[0])
-                price = (offer[1] * numOffer) + (remaining * regularPrice)
-                return price
+                break   
+            else: 
+                if qtyRemaining > 0: # can claim at least one offer
+                    numOffer = qtyRemaining // offer[0]
+                    qtyRemaining -= (numOffer*offer[0]) 
+                    # price = (offer[1] * numOffer) + (remaining * regularPrice)
+                    totalPrice += (offer[1] * numOffer)
 
-    def priceCalculator(self, item, qty):
+        if qtyRemaining > 0: # include any remaining items
+            totalPrice += (qtyRemaining * regularPrice)
+
+        freebies = offer[2]
+        if freebies != (): # some freebies
+            if freebies[0] in itemsOrdered.keys():
+
+
+    def priceCalculator(self, item, qty,itemsOrdered):
         if (qty < 1): # illegal input
             return -1
 
         if item not in self.specialOffers: # no special offers, sell at regular price 
             return self.prices[item] * qty
     
-        return self.offerCalculator(item, qty)
+        return self.offerCalculator(item, qty, itemsOrdered)
     
     def checkout(self, skus): # skus = unicode string
         if skus == "": # illegal input
@@ -60,7 +71,7 @@ class CheckoutSolution:
 
         totalCheckoutVal = 0
         for item, qty in itemsOrdered.items():
-            val = self.priceCalculator(item,qty)
+            val = self.priceCalculator(item,qty,itemsOrdered)
             if val != -1:
                 totalCheckoutVal += val 
             else:
@@ -70,3 +81,4 @@ class CheckoutSolution:
 
 supermarket = CheckoutSolution()
 print(supermarket.checkout(""))
+
