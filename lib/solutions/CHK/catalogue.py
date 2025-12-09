6a3e306fb17854ed73f5d3ffb6062fd4bee2ab0c
sbx_user1051 <sbx_user1051@169.254.54.117>
2025-12-09 13:38:07 +0000

    Tue Dec 09 13:38:07 UTC 2025

---
+++ lib/solutions/CHK/catalogue.py
@@ -0,0 +1,17 @@
+
+
+class SupermarketCatalogue():
+    prices = {
+        "A" : 50, "B" : 30, "C" : 20,
+        "D" : 15,
+        "E" : 40,
+        "F" : 10,
+        "G" : 20,
+        "H" : 10,
+        "I" : 35,
+        "J" : 60,
+        "K" : 80
+    }
+
+    def getPrices():
+        return self.prices
