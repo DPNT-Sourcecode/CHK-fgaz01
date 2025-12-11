f466a3ab73cbc9634d7ab088ad28859188444038
sbx_user1051 <sbx_user1051@169.254.44.213>
2025-12-11 15:06:53 +0000

    Thu Dec 11 15:06:53 UTC 2025

--- lib/solutions/CHK/checkout_tests.py
+++ lib/solutions/CHK/checkout_tests.py
@@ -4,26 +4,37 @@ import unittest
 class TestCheckout(unittest.TestCase):
     supermarket = CheckoutSolution()
 
-    def test_singleItemBasket(self):
+    def test_single_item_basket(self):
         self.assertEqual(self.supermarket.checkout("AAA"),130)
 
-    def test_singleItemBasket2(self):
+    def test_single_item_basket_2(self):
         self.assertEqual(self.supermarket.checkout("CC"),40)
 
-    def test_multiItemBasket(self):
+    def test_multi_item_basket(self):
         self.assertEqual(self.supermarket.checkout("ABCDEABCDE"),280)
 
-    def test_multiItemBasket2(self):
+    def test_multi_Item_basket_2(self):
         self.assertEqual(self.supermarket.checkout("CCADDEEBBA"),280)
 
-    def test_multiItemBasketWithDeals(self):
+    def test_multi_item_basket_with_deals(self):
         self.assertEqual(self.supermarket.checkout("AAAAAEEBAAABB"),455)
 
-    def test_multiItemBasketWithDeals2(self):
+    def test_multi_item_basket_with_deals_2(self):
         self.assertEqual(self.supermarket.checkout("ABCDECBAABCABBAAAEEAA"),665)
 
-    def test_buyOneGetOneFree(self):
-        self.assertEqual(self.supermarket.checkout("CCADDEEBBA"),280)
+    def test_buy_one_get_one_free(self):
+        self.assertEqual(self.supermarket.checkout("FFFFFF"),40)
 
-     def test_multiItemBasket2(self):
-        self.assertEqual(self.supermarket.checkout("CCADDEEBBA"),280)
+    def test_buy_one_get_one_free_2(self):
+        self.assertEqual(self.supermarket.checkout("FFABCDECBAABCABBAAAEEAAFF"),695)
+
+    def test_multi_deals(self):
+        self.assertEqual(self.supermarket.checkout("NNNM"),120)
+    
+    def test_multi_deals_2(self):
+        self.assertEqual(self.supermarket.checkout("NNNNM"),160)
+
+    def test_multi_deals_3(self):
+        self.assertEqual(self.supermarket.checkout("NNNNNNMM"),240)
+
+
